8985687ffe25879e11d432ea822b3b6c3668203f
Ryan Szymkiewicz <szymkie1@uic.edu>
2014-04-09 15:42:21 -0500

    4/9/14
    Added Basic GUI elements

---
+++ chatClient/src/README.txt
@@ -0,0 +1,8 @@
+CS342 Project 4 & 5
+===================
+University of Illinois at Chicago
+CS342 - Prof Troy
+
+Team Members:
+Ryan Szymkiewicz
+Joshua Maravelias
---
+++ chatClient/src/chatStart.java
@@ -0,0 +1,25 @@
+/*****************************
+ * chatStart.java
+ * Written by Ryan Szymkiewicz for CS 342 Project 4
+ * This class contains the main method and will start the 
+ * GUI for the client while completely exiting the window
+ * when "x" is pressed
+ */
+
+
+
+
+
+import javax.swing.*;
+
+
+public class chatStart{
+
+	public static void main(String[] args){
+
+		chatGUI gui = new chatGUI();
+		gui.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+	}
+
+}
